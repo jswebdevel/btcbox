45d0796d428cccda29318d9ef8629ec7495d6e4c
jswebdevel <admin@jswebdevelopment.net>
2014-10-16 22:12:55 -0600

    Create timer.js
    A java script timer just so the user knows how much time remains before redirect occurs.

---
+++ timer.js
@@ -0,0 +1,16 @@
+var seconds = 60;
+function secondPassed() {
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('countdown').innerHTML = "Ok!";
+    } else {
+        seconds--;
+    }
+}
+var countdownTimer = setInterval('secondPassed()', 1000);
